19e6c58ea299f64e3726126d09e787aab36d7356
Mitchell Hewes <me@mitcdh.com>
2016-10-16 08:18:49 +1100

    fully qualified php in crown job

--- files/wp-cron.sh
+++ files/wp-cron.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "1 * * * * php -q /www/public/wp-cron.php >/dev/null 2>&1" | crontab -u www-data -
+echo "1 * * * * /usr/bin/php -q /www/public/wp-cron.php >/dev/null 2>&1" | crontab -u www-data -
 crond -b -d 8
